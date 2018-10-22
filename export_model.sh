b858019db8a9908ff0e741d5c4316aff7ed5240e
countertop <synthesisproject@mit.edu>
2018-10-22 15:02:59 -0400

    aggregate to find on full dataset

--- export_model.sh
+++ export_model.sh
@@ -1,3 +1,1 @@
-python bin/dump_weights.py \
-    --save_dir checkpoints/
-    --outfile checkpoints/output_weights.hdf5
+python bin/dump_weights.py --save_dir checkpoints/ --outfile checkpoints/output_weights.hdf5
