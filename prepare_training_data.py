e00b34faba98fea8eb3b3080b6e307fc2db0a421
Edward Kim <eddotman@gmail.com>
2018-10-22 17:05:29 -0400

    update training data script

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -1,4 +1,5 @@
 import spacy
+import os
 from pymongo import MongoClient
 
 def main():
@@ -7,8 +8,16 @@ def main():
     papers = client.predsynth.papers.find(no_cursor_timeout=True)
     
     for i, paper in enumerate(papers):
+        doi = paper["doi"]
+        safe_doi = ''.join(c for c in doi if c not in ["/", ".", "(", ")"])
+        filename = f"training_data/{safe_doi}.txt"
+
+        if os.path.isfile(filename):
+            continue
+
         full_text = "\n".join([p["text"] for p in paper["paragraphs"]])
-        with open(f"training_data/{i}.txt", "w") as f:
+
+        with open(filename, "w") as f:
             for sent in nlp(full_text[:500000]).sents:
                 f.write(f"{' '.join([t.text for t in sent])}\n")
 
