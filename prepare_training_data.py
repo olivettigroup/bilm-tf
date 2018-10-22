fbff872e794954f572f1344a35e2345655cf250c
countertop <synthesisproject@mit.edu>
2018-10-22 13:22:57 -0400

    add length limit

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -9,7 +9,7 @@ def main():
     for i, paper in enumerate(papers):
         full_text = "\n".join([p["text"] for p in paper["paragraphs"]])
         with open(f"training_data/{i}.txt", "w") as f:
-            for sent in nlp(full_text).sents:
+            for sent in nlp(full_text[:500000]).sents:
                 f.write(f"{' '.join([t.text for t in sent])}\n")
 
 if __name__ == "__main__":
