bfc80957f130d05b95d60a653aefc3747cbc7f49
Edward Kim <eddotman@gmail.com>
2018-10-23 15:23:18 -0400

    fix leading slash error

--- bilm/training.py
+++ bilm/training.py
@@ -1076,11 +1076,11 @@ def dump_weights(tf_save_dir, outfile):
     def _get_outname(tf_name):
         outname = re.sub(':0$', '', tf_name)
         outname = outname.lstrip('lm/')
-        outname = re.sub('/rnn/', '/RNN/', outname)
+        outname = re.sub('rnn/', 'RNN/', outname)
         outname = re.sub('/multi_rnn_cell/', '/MultiRNNCell/', outname)
         outname = re.sub('/cell_', '/Cell', outname)
         outname = re.sub('/lstm_cell/', '/LSTMCell/', outname)
-        if '/RNN/' in outname:
+        if 'RNN/' in outname:
             if 'projection' in outname:
                 outname = re.sub('projection/kernel', 'W_P_0', outname)
             else:
